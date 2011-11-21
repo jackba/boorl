b7a397fcd6d77bd639433cc880c8e3180d3b5eae
Andrija.Vucinic <Andrija.Vucinic@gmail.com>
2011-11-21 19:52:41 +0000

    Added config settings for Google Analytics.

--- include/config/config.php
+++ include/config/config.php
@@ -6,13 +6,16 @@
 
 	// Is BooRL installed, or it's the first time running it
 	define("INSTALL", "##0##");
-    // Database host
-    define("HOST", "##1##");
-    // Database name
-    define("DATABASE", "##2##");
-    // Database username
-    define("USERNAME", "##3##");
-    // Database password
+  // Database host
+  define("HOST", "##1##");
+  // Database name
+  define("DATABASE", "##2##");
+  // Database username
+  define("USERNAME", "##3##");
+  // Database password
 	define("PASSWORD", "##4##");
-
+	// Domain
+	define("DOMAIN", "##5##");
+	// Google Analytics Code
+	define("GA_CODE", "##6##");
 ?>
